0e518a479a22951a986171f7748ecade6adee2d5
komat0 <111368527+komat0@users.noreply.github.com>
2023-04-27 16:41:54 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+        staff.sort(Comparator.comparing(Employee::getSalary)
+                .thenComparing((e1, e2) -> e1.getName().compareTo(e2.getName())));
+
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
